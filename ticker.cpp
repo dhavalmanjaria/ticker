ae12c8cb35f1495d550317661d0784477deed228
DhavaL <dhv2712@gmail.com>
2014-06-06 20:16:31 +0530

    Commented code

--- ticker.cpp
+++ ticker.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-	using namespace std;
+	using namespace std; // Make things easier
 
-	string fileName;
+	string fileName; // Filename of the file to be read from
 
 	if (argc != 2) {
 		cout << "Usage: " << endl;
@@ -18,22 +18,25 @@ int main(int argc, char *argv[])
 
 	ifstream in(fileName);
 
-	string buf, fullString;
+	string buf;
+	// buf will hold the output from i3status
+	
 	vector<string> lines;
+	// This will hold all the lines from the file
 
 	while(in.good()) {
 		getline(in,buf);
 		lines.push_back(buf);
 	}	
 	in.close();
+	// We do this so that we don't read the file each time we tick.
 
 
 	while(cin.good()) {
-
 		for (unsigned int i = 0; lines.size() > i; i++) {
-			getline(cin,buf);
+			getline(cin,buf); // Get i3status input into a line
 			cout << lines[i] << " | " << buf;
-			cout << flush;
+			cout << flush; // flush output so that it's shown immediately.
 		}
 		
 	}
